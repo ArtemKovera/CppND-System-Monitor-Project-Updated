31cd9057058f386b5f2a5124c1412301433eb845
Artem Kovera <artemkovera@gmail.com>
2021-05-19 16:20:14 +0300

    Utilization() method

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,47 @@
 #include "processor.h"
+#include "linux_parser.h"
+#include <unistd.h>
+#include <fstream>
+#include <sstream>
+  
+using namespace LinuxParser;
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+//returns 0 if the file is unable to be open
+float Processor::Utilization()
+{   
+    std::ifstream file(kProcDirectory + kStatFilename);
+
+    if(file.is_open())
+    {
+        int total = 0;
+        int jiffyComponent;
+        int idle;
+        int column = 0;
+        float idleFruction;
+        std::string line;
+
+        std::getline(file, line);
+        line.erase(0, 5);
+        std::istringstream stream(line);
+        
+        
+        while(stream >> jiffyComponent)
+        {
+            total += jiffyComponent;
+            column++;
+            if(column==4)
+                idle = jiffyComponent;
+
+        }
+        
+        idleFruction = (float)idle/total;
+
+        return (1.0 - idleFruction);        
+    }
+    else
+    {
+        return 0.0;
+    }
+      
+}
